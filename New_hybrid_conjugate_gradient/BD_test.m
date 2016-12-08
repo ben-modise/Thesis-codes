794c691038deac7984ca53932ba22b611813cc40
Ben Modise <thuso@aims.ac.za>
2016-12-08 13:00:37 +0200

    began work on the main test script. Functioning for problem 1

--- New_hybrid_conjugate_gradient/BD_test.m
+++ New_hybrid_conjugate_gradient/BD_test.m
@@ -4,41 +4,62 @@
 %problem 1
 %setting initial points.
 dim=10000;
+%vname=@(x) inputname(1); %to be able to convert variable names to strings
+
 x1 = ones(dim,1); x2 = 100*ones(dim,1); x3 = 0.1*ones(dim,1);
 x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 
+init = [x1,x2,x3,x4,x5,x6];
+fprintf('     Testing problem 1\n');
+fprintf('---------------------------------\n')
+n= size(init,2); %gives the number of test points
+output = fopen('Results/Problem1.csv','wt');
+fprintf(output,'\n Results for Problem 1 in the hybrid CG proj scheme\n');
+fprintf(output,'Initial, Dim, BNN, , BD, , \n');
+fprintf(output,',,Iterations,Function Evals,Iterations,Function Evals\n');
+%strcat to concatenate strings
+for i=1:n
+   [~,k,numf,~,iflag] = BD(init(:,i), @problem1, @P_Rplus); 
+   [~,k2,numf2,~,iflag2] = BNN(init(:,i), @problem1, @P_Rplus);
+   %str_i = vname(i);
+   xi = strcat('x',num2str(i));
+   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
+   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
+end
+
+fclose(output);
 %TODO: iterate through the initial points and run our algorithm 
 %and store or print to file the data points.
-[~,k2,numf2,~, ~] = BD(x1, @problem1, @P_Rplus);
 
-%problem 2
-dim=10000;
-x1 = ones(dim,1); % x2 = 100*ones(dim,1); is outside the constraint, 
-%projection onto the convex set yields x1
-x3 = 0.1*ones(dim,1);
-x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 
-[~,k1,numf1,~, ~] = BNN(x0, @problem2, @P_L1Rplus);
-
-%problem3
-dim=10000;
-x1 = ones(dim,1); x2 = -ones(dim,1); x3 = 0.1*ones(dim,1);
-x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
-
-[x,k2,numf2,~,iflag] = BNN(x0, @problem3);
-
-%problem4
-dim = 10000;
-%x1 = ones(dim,1);  %this is the solution we are looking for.
-x2 = zeros(dim,1); x3 = 0.1*ones(dim,1);
-x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
-
-[x,k2,numf2,~,iflag] = BD(x4, @problem4);
-%problem 5
-dim =100;
-
-clear functions;
-x1 = ones(dim,1); x2 = 10*ones(dim,1); x3 = 0.1*ones(dim,1);
-x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
-[x,k2,numf2,~, iflag] = BD(x0, @problem5, @P_Rplus);
+% %problem 2
+% dim=10000;
+% x1 = ones(dim,1); % x2 = 100*ones(dim,1); is outside the constraint, 
+% %projection onto the convex set yields x1
+% x3 = 0.1*ones(dim,1);
+% x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
+% 
+% [~,k1,numf1,~, ~] = BNN(x0, @problem2, @P_L1Rplus);
+% 
+% %problem3
+% dim=10000;
+% x1 = ones(dim,1); x2 = -ones(dim,1); x3 = 0.1*ones(dim,1);
+% x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
+% 
+% [x,k2,numf2,~,iflag] = BNN(x0, @problem3);
+% 
+% %problem4
+% dim = 10000;
+% %x1 = ones(dim,1);  %this is the solution we are looking for.
+% x2 = zeros(dim,1); x3 = 0.1*ones(dim,1);
+% x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
+% 
+% [x,k2,numf2,~,iflag] = BD(x4, @problem4);
+% %problem 5
+% dim =100;
+% 
+% clear functions;
+% x1 = ones(dim,1); x2 = 10*ones(dim,1); x3 = 0.1*ones(dim,1);
+% x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
+% [x,k2,numf2,~, iflag] = BD(x0, @problem5, @P_Rplus);
 
