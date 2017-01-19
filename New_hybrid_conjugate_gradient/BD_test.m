d29a3b405a893ab253bc2963e06c41370d3353ee
Ben Modise <thuso@aims.ac.za>
2017-01-19 15:26:49 +0200

    ignored varargin file, modified BD_test by excecuting file printing to a separate function

--- New_hybrid_conjugate_gradient/BD_test.m
+++ New_hybrid_conjugate_gradient/BD_test.m
@@ -12,24 +12,7 @@ x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 init = [x1,x2,x3,x4,x5,x6];
 fprintf('     Testing problem 1\n');
 fprintf('---------------------------------\n')
-n= size(init,2); %gives the number of test points
-output = fopen('Results/Problem1.csv','wt');
-%fprintf(output,'\n Results for Problem 1 in the hybrid CG proj scheme\n');
-fprintf(output,'Initial, Dim,Iterations,Function Evals,Iterations,Function Evals\n');
-fprintf(output,' , , BNN, , BD, , \n');
-%strcat to concatenate strings
-for i=1:n
-   [~,k,numf,~,iflag] = BD(init(:,i), @problem1, @P_Rplus); 
-   [~,k2,numf2,~,iflag2] = BNN(init(:,i), @problem1, @P_Rplus);
-   %str_i = vname(i);
-   xi = strcat('x',num2str(i));
-   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
-   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
-end
-
-fclose(output);
-
-
+writetofile(init, @problem1, @P_Rplus);
 
 %problem 2
 dim=10000;
@@ -42,22 +25,7 @@ x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 init = [x1,x2,x4,x5,x6];%,x3 exceeds zk trial iterations for BD
 fprintf('     Testing problem 2\n');
 fprintf('---------------------------------\n')
-n= size(init,2); %gives the number of test points
-output = fopen('Results/Problem2.csv','wt');
-%fprintf(output,'\n Results for Problem 2 in the hybrid CG proj scheme\n');
-fprintf(output,'Initial, Dim,Iterations,Function Evals,Iterations,Function Evals\n');
-fprintf(output,',, BNN,, BD,\n');
-%strcat to concatenate strings
-for i=1:n
-   [~,k,numf,~,iflag] = BD(init(:,i), @problem2,@P_L1Rplus); 
-   [~,k2,numf2,~,iflag2] = BNN(init(:,i), @problem2,@P_L1Rplus);
-   %str_i = vname(i);
-   xi = strcat('x',num2str(i));
-   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
-   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
-end
-
-fclose(output);
+writetofile(init, @problem2,  @P_L1Rplus);
 
 %problem3
 dim=10000;
@@ -66,22 +34,7 @@ x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 init = [x1,x2,x3,x4,x5,x6];
 fprintf('     Testing problem 3\n');
 fprintf('---------------------------------\n')
-n= size(init,2); %gives the number of test points
-output = fopen('Results/Problem3.csv','wt');
-%fprintf(output,'\n Results for Problem 3 in the hybrid CG proj scheme\n');
-fprintf(output,'Initial, Dim,Iterations,Function Evals,Iterations,Function Evals\n');
-fprintf(output,',, BNN,, BD,\n');
-%strcat to concatenate strings
-for i=1:n
-   [~,k,numf,~,iflag] = BD(init(:,i), @problem3); 
-   [~,k2,numf2,~,iflag2] = BNN(init(:,i), @problem3);
-   %str_i = vname(i);
-   xi = strcat('x',num2str(i));
-   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
-   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
-end
-
-fclose(output);
+writetofile(init, @problem3);
 
 %problem4
 dim = 10000;
@@ -92,22 +45,7 @@ x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 init = [x1,x2,x3,x4,x5,x6];
 fprintf('     Testing problem 4\n');
 fprintf('---------------------------------\n')
-n= size(init,2); %gives the number of test points
-output = fopen('Results/Problem4.csv','wt');
-%fprintf(output,'\n Results for Problem 4 in the hybrid CG proj scheme\n');
-fprintf(output,'Initial, Dim,Iterations,Function Evals,Iterations,Function Evals\n');
-fprintf(output,',, BNN,, BD,\n');
-%strcat to concatenate strings
-for i=1:n
-   [~,k,numf,~,iflag] = BD(init(:,i), @problem4); 
-   [~,k2,numf2,~,iflag2] = BNN(init(:,i), @problem4);
-   %str_i = vname(i);
-   xi = strcat('x',num2str(i));
-   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
-   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
-end
-
-fclose(output);
+writetofile(init, @problem4);
 
 
 %problem 5
@@ -119,22 +57,5 @@ x4a = ones(dim,1); x4b = (1:dim)';x4=x4a./x4b; x5 = flip(x4); x6 = x1 - x4;
 init = [x1,x2,x3,x4,x5,x6];
 fprintf('     Testing problem 5\n');
 fprintf('---------------------------------\n')
-n= size(init,2); %gives the number of test points
-output = fopen('Results/Problem5.csv','wt');
-%fprintf(output,'\n Results for Problem 5 in the hybrid CG proj scheme\n');
-fprintf(output,'Initial, Dim,Iterations,Function Evals,Iterations,Function Evals\n');
-fprintf(output,',, BNN,, BD,\n');
-%strcat to concatenate strings
-for i=1:n
-   [~,k,numf,~,iflag] = BD(init(:,i), @problem5,@P_Rplus); 
-   [~,k2,numf2,~,iflag2] = BNN(init(:,i), @problem5,@P_Rplus);
-   %str_i = vname(i);
-   xi = strcat('x',num2str(i));
-   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
-   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
-end
-
-fclose(output);
-
-% [x,k2,numf2,~, iflag] = BD(x0, @problem5, @P_Rplus);
+writetofile(init, @problem5,@P_Rplus);
 
---
+++ New_hybrid_conjugate_gradient/writetofile.m
@@ -0,0 +1,23 @@
+function writetofile(init,varargin)
+%writes to csv file the output 
+%usage: writetofile(init, Problem(objective function), Projection)
+
+Problem = varargin{1};
+n= size(init,2); %gives the number of test points
+dim = size(init,1);
+outputfile = strcat('Results/',char(Problem),'.csv');
+output = fopen(outputfile,'wt');
+%fprintf(output,'\n Results for Problem 1 in the hybrid CG proj scheme\n');
+fprintf(output,'Initial, Dim,Iterations,Function Evals,Iterations,Function Evals\n');
+fprintf(output,' , , BNN, , BD,\n');
+%strcat to concatenate strings
+for i=1:n
+   [~,k,numf,~,~] = BD(init(:,i), varargin{:}); 
+   [~,k2,numf2,~, ~] = BNN(init(:,i), varargin{:});
+   %str_i = vname(i);
+   xi = strcat('x',num2str(i));
+   %fprintf(output,'%2d\t\t %3d\t%4d\t%10.8f\t\t%12.8f\n',iflag,iter,numf,tsec,gnorm);
+   fprintf(output,'%s,%2d,%d,%d, %d,%d\n',xi,dim,k2,numf2,k,numf);
+end
+
+fclose(output);
